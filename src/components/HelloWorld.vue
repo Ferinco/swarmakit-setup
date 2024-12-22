51905f53e71b402907003171af8ba34c9539256a
Ferinco <emmanuelferrum003@gmail.com>
2024-12-22 11:36:44 +0100

    setup complete

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import RightDiv from './RightDiv.vue'
+
+
+</script>
+
+<template>
+  <div class="parent-div">
+    <div class="child child-1">
+      <div class="child-1-content">
+        <h1>I'm a Samurai!</h1>
+      </div>
+    </div>
+    <div class="child child-2">
+      <RightDiv />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.parent-div {
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+}
+
+.child {
+  text-align: center;
+  padding: 20px;
+}
+
+.child-1 {
+  flex: 3;
+  background-image: url('/2150169179.jpg');
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  padding: 0;
+  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px !important;
+}
+
+.child-2 {
+  flex: 2;
+  background-color: white;
+}
+
+.child-1-content {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  justify-content: end;
+  background: linear-gradient(to top right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0));
+  align-items: end;
+
+}
+
+h1 {
+  display: flex;
+  justify-content: end;
+  padding: 5px 20px;
+  font-size: 4.2em;
+  line-height: 1.1;
+}
+@media (max-width: 864px) {
+  .child-1 {
+    display: none;
+  }
+}
+</style>
---
+++ src/components/RightDiv.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { CardImage, CardBody, SearchBar, Button } from '@swarmakit/vue'
+
+</script>
+
+<template>
+    <div class="form-div">
+        <div class="search-bar">
+            <SearchBar placeholder="check other devs"/>
+        </div>
+        <div class="content-div">
+            <CardImage src="/2150169179.jpg" alt="A random image"/>
+            <CardBody>
+                <h1>Please Employ Me🥺</h1>
+                <p>I built a simple webpage with Vue + Vite and Swarmakit library... The <b>search bar</b> above, the <b>card body</b> wrapping these texts and the <b>buttons</b> below are all from  </p> ⬇️ <br/> <a href="https://www.npmjs.com/package/@swarmakit/vue?activeTab=readme" target="_blank">Swarmakit Library</a>
+            </CardBody>
+    </div>
+    <Button type="primary">
+        <a href="https://www.linkedin.com/in/oluwaferanmi-emmanuel-280746270/" target="_blank" class="link">Employ Me</a>
+    </Button>
+    <Button type="primary" disabled>
+        don't employ - disabled button
+    </Button>
+    
+</div>
+
+</template>
+
+<style scoped>
+.form-div {
+    display: flex;
+    flex-direction: column;
+    color: black;
+    background-color: white;
+    gap: 10px;
+    height: 100%;
+}
+.search-bar{
+    display: flex;
+    justify-content: end;
+}
+.try {
+    height: 300px;
+}
+.content-div{
+    display: flex;
+    margin: auto;
+}
+.link{
+    text-decoration: none;
+    color: black;
+    font-size: 18px;
+    font-weight: 500;
+}
+h1{
+    font-size: 3.2em;
+  line-height: 1.1;
+}
+</style>
